4f12a5b33c9517beaad0d116d6deab61ef7f770f
thryec <thryec@gmail.com>
2022-06-09 21:15:18 +0800

    added create ETH stream function

--- contracts/Patreon.sol
+++ contracts/Patreon.sol
@@ -20,7 +20,6 @@ contract Patreon is ReentrancyGuard {
         uint256 stopTime;
         address recipient;
         address sender;
-        address tokenAddress;
         bool isEntity;
     }
 
@@ -35,5 +34,40 @@ contract Patreon is ReentrancyGuard {
         );
         (bool success, ) = payable(_recipient).call{value: msg.value}("");
         require(success, "Ether not sent successfully");
+    }
+
+    function createETHStream(
+        address recipient,
+        uint256 startTime,
+        uint256 stopTime
+    ) public payable returns (uint256) {
+        uint256 depositAmount = msg.value;
+        require(recipient != address(0x00), "stream to the zero address");
+        require(recipient != address(this), "stream to the contract itself");
+        require(recipient != msg.sender, "stream to the caller");
+        require(depositAmount > 0, "deposit is zero");
+        require(
+            startTime >= block.timestamp,
+            "start time before block.timestamp"
+        );
+        require(stopTime > startTime, "stop time before the start time");
+
+        uint256 duration = stopTime - startTime;
+        uint256 _ratePerSecond = depositAmount / duration;
+
+        uint256 currentStreamId = _streamIds.current();
+        streams[currentStreamId] = Stream({
+            remainingBalance: depositAmount,
+            deposit: depositAmount,
+            isEntity: true,
+            ratePerSecond: _ratePerSecond,
+            recipient: recipient,
+            sender: msg.sender,
+            startTime: startTime,
+            stopTime: stopTime
+        });
+
+        _streamIds.increment();
+        return currentStreamId;
     }
 }
