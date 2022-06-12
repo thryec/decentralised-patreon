16fee9cd3868f86a60dd7cc4207c25c62853c5d5
thryec <thryec@gmail.com>
2022-06-12 12:39:12 +0800

    change subcription to months; fix as  monthly frequency

--- next/components/Subscribe.tsx
+++ next/components/Subscribe.tsx
@@ -3,6 +3,7 @@ import { useAccount } from "wagmi";
 import { shortenAddress } from "../helpers";
 
 const Subscribe = () => {
+  // once a month "subscription"
   const { data } = useAccount();
 
   return (
@@ -23,13 +24,13 @@ const Subscribe = () => {
         ETH
       </div>
       <div>
-        <label htmlFor="frequency">Duration:</label>
+        <label htmlFor="duration">Duration:</label>
         <input
           type="text"
           className="border border-slate-200 rounded-md"
           placeholder="0"
         />{" "}
-        Days
+        Months
       </div>
       <button className="px-4 py-2 bg-blue-500 rounded-md text-white font-bold block">
         Tip
