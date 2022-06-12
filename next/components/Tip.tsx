f10d5fef07f53a6491287d6c7c31d09df10fd6b5
thryec <thryec@gmail.com>
2022-06-12 12:06:35 +0800

    display connected address in tip modal

--- next/components/Tip.tsx
+++ next/components/Tip.tsx
@@ -1,8 +1,18 @@
+import { useAccount } from "wagmi";
+
 const Tip = () => {
+  const { data } = useAccount();
+
+  const shortenAddress = (str: any) => {
+    return str.substring(0, 5) + "..." + str.substring(str.length - 4);
+  };
+
   return (
     <div>
       <h2>To: creator address</h2>
-      <h2>From: user account</h2>
+      <h2>
+        From: <code>{shortenAddress(data?.address)}</code>
+      </h2>
       <label htmlFor="amount">Amount to tip: </label>
       <input
         type="text"
